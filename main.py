c37386209b73eeee35fd9f55a43d4caa73f09b16
Danya <danya.shishkov@icloud.com>
2026-04-19 16:36:17 +0300

    ball, player update
    добавил мяч, движение меча, поменял клавиши в движении ракеток и сделал движение ракеток

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from pygame import*
+from pygame import *
 
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+    def __init__(self, player_image, player_x, player_y, player_speed, widht, height):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.image = transform.scale(image.load(player_image), (widht, height))
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
@@ -13,17 +13,24 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-    def update(self):
+    def update_r(self):
         keys = key.get_pressed()
-        if (keys[K_UP] or keys[K_W])and self.rect.y > 5:
-            self.rect.x -= self.speed
-        if (keys[K_DOWN] or keys[K_S]) and self.rect.x < win_height - 80:
-            self.rect.x += self.speed
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 155:
+            self.rect.y += self.speed
 
-back = (200,255,255)
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 155:
+            self.rect.y += self.speed
+
+back = (200, 255, 255)
 win_height = 500
 win_width = 600
-window = display.set_mode((win_width,win_height))
+window = display.set_mode((win_width, win_height))
 window.fill(back)
 
 game = True
@@ -31,12 +38,27 @@ finish = False
 clock = time.Clock()
 FPS = 60
 
+racket1 = Player('racket.png', 30, 200, 4, 50, 150)
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+
+speed_x = 3
+speed_y = 3
+
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
     if finish != True:
         window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
     display.update()
     clock.tick(FPS)
 
