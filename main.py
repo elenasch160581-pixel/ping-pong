b743c1aa2696196b38444953fd4323c57ac9b1f0
Danya <danya.shishkov@icloud.com>
2026-04-26 15:34:41 +0300

    ping pong
    добавил проигрыши игроков

--- main.py
+++ main.py
@@ -42,6 +42,11 @@ racket1 = Player('racket.png', 30, 200, 4, 50, 150)
 racket2 = Player('racket.png', 520, 200, 4, 50, 150)
 ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
 
+font.init()
+font = font.Font (None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180,0,0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180,0,0))
+
 speed_x = 3
 speed_y = 3
 
@@ -59,6 +64,17 @@ while game:
             speed_x *= -1
         if ball.rect.y < 0 or ball.rect.y > win_height-50:
             speed_y *= -1
+
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200,200))
+            game_over = True
+            
+        if ball.rect.x > win_width-50:
+            finish = True
+            window.blit(lose2, (200,200))
+            game_over = True
+
 
 
         racket1.reset()
