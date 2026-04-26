e8243eea9639c11762ac435390b9bd4628b4fbc7
Danya <danya.shishkov@icloud.com>
2026-04-26 16:45:26 +0300

    0.1
    добавил счёт

--- main.py
+++ main.py
@@ -16,16 +16,16 @@ class GameSprite(sprite.Sprite):
 class Player(GameSprite):
     def update_r(self):
         keys = key.get_pressed()
-        if keys[K_w] and self.rect.y > 5:
+        if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y < win_height - 155:
+        if keys[K_DOWN] and self.rect.y < win_height - 155:
             self.rect.y += self.speed
 
     def update_l(self):
         keys = key.get_pressed()
-        if keys[K_UP] and self.rect.y > 5:
+        if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < win_height - 155:
+        if keys[K_s] and self.rect.y < win_height - 155:
             self.rect.y += self.speed
 
 back = (200, 255, 255)
@@ -44,14 +44,20 @@ racket2 = Player('racket.png', 520, 200, 4, 50, 150)
 ball = GameSprite('tenis_ball.png', randint(175, 225), randint(175, 225), 4, 50, 50)
 
 font.init()
-font = font.Font (None, 35)
+font = font.Font(None, 35)
 lose1 = font.render('PLAYER 1 LOSE!', True, (180,0,0))
 lose2 = font.render('PLAYER 2 LOSE!', True, (180,0,0))
 
-rand_speed = [-1, 1]
+rand_speed = [-1,1]
 
-speed_x = 3 * rand_speed[randint(0, 1)]
-speed_y = 3 * rand_speed[randint(0, 1)]
+speed_x = 3 * rand_speed[randint(0,1)]
+speed_y = 3 * rand_speed[randint(0,1)]
+
+score1 = 0
+score2 = 0
+
+score = font.render(f'Счёт: {str(score1)}/{score2}', True, (0,0,0))
+
 
 while game:
     for e in event.get():
@@ -65,26 +71,38 @@ while game:
         ball.rect.y += speed_y
         if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
             speed_x *= -1
+
         if ball.rect.y < 0 or ball.rect.y > win_height-50:
             speed_y *= -1
 
         if ball.rect.x < 0:
-            finish = True
-            window.blit(lose1, (200,200))
-            game_over = True
-            
+            score2 += 1
+            score = font.render(f'Счёт: {str(score1)}/{score2}', True, (0,0,0))
+            ball.rect.x = randint(175, 255)
+            ball.rect.y = randint(175, 255)
+            speed_x *= rand_speed[randint(0,1)]
+            speed_y *= rand_speed[randint(0,1)]
+        
         if ball.rect.x > win_width-50:
+            score1 += 1
+            score = font.render(f'Счёт: {str(score1)}/{score2}', True, (0,0,0))
+            ball.rect.x = randint(175, 255)
+            ball.rect.y = randint(175, 255)
+            speed_x *= rand_speed[randint(0,1)]
+            speed_y *= rand_speed[randint(0,1)]
+
+        if score1 > 4 or score2 > 4:
             finish = True
-            window.blit(lose2, (200,200))
-            game_over = True
-
-
-
+            ball.rect.x = 400
+            ball.rect.y = 400
+            if score1 > score2:
+                window.blit(lose2, (200, 200))
+            else:
+                window.blit(lose1, (200, 200))
+        
+        window.blit(score, (250, 0))
         racket1.reset()
         racket2.reset()
         ball.reset()
     display.update()
     clock.tick(FPS)
-
-
-    
